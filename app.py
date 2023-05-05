70147a4d1a683c8ef328569dc3b5c31f03f1fa67
or-navon <or@cypago.com>
2023-05-05 19:01:41 +0300

    Adding joing room functionality + Adding Game database model object + start game request structure + delete db.py file

--- app.py
+++ app.py
@@ -2,24 +2,41 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from sqlalchemy import Column
+from sqlalchemy.types import String, JSON, Boolean
+
 from uuid import uuid4
 
 # Creating the flask app
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///game.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-
 db = SQLAlchemy(app)
-migrate = Migrate(app, db)
 
 class User(db.Model):
-    id = db.Column(db.String(128), primary_key=True)
-    name = db.Column(db.String(128))
+    id = Column(String(128), primary_key=True)
+    name = Column(String(128))
 
 class Room(db.Model):
-    id = db.Column(db.String(128), primary_key=True)
-    name = db.Column(db.String(128))
+    id = Column(String(128), primary_key=True)
+    name = Column(String(128))
+    dealer = Column(String(128))
+    player = Column(String(128))
+
+class Game(db.Model):
+    id = Column(String(128), primary_key=True)
+    room = Column(String(128))
+
+    # Status can be one of some options - playing, dealer_won, player_won, tie
+    status = Column(String(128))
+
+    # deck of cards, json list containing cards in this fomat: [{suit: "hearts", value: "A"}, ...]
+    deck_json = Column(JSON) 
+    dealer_hand_json = Column(JSON)
+    player_hand_json = Column(JSON)
+    current_turn = Column(String(128))
+
+migrate = Migrate(app, db)
 
 
 @app.route('/room', methods=['POST'])
@@ -27,21 +44,54 @@ def create_room():
     # CONNECT DATABASE, create new room
     # create new room in the database
     # return room id
+    user_id = 123
     new_room_id = uuid4()
-    new_room = Room(id=str(new_room_id), name=f"room-{new_room_id}")
+    new_room = Room(id=str(new_room_id), name=f"room-{new_room_id}", dealer=user_id)
     db.session.add(new_room)
     db.session.commit()
     return str(new_room_id)
 
 
-
-
-
-@app.route('/')
-def index():
-    print(app.config['SQLALCHEMY_DATABASE_URI'])
-    # return status code 200
+@app.route('/join/<room_number>', methods=['POST'])
+def join_player_to_room(room_number):
+    # CONNECT DATABASE, create new room
+    # create new room in the database
+    # return room id
+    user_id = "123"
+    room = db.session.query(Room).filter_by(id=room_number).first()
+    if room.player is not None:
+        return "Room is full", 400
+    if room.dealer == user_id:
+        return "You are the dealer, you can not join your own room", 400
+    room.player = user_id
+    db.session.commit()
     return "OK"
+
+
+@app.route('/start/<room_number>', methods=['POST'])
+def start_game(room_number):
+    user_id = 123
+    # Check if the user is the dealer of this room - TODO: Lilach should implement this
+
+    # Check if the room is full - TODO: Lilach should implement this
+
+
+    # In case both checks passed, create a new game in the database
+    
+    room = db.session.query(Room).filter_by(id=room_number).first()
+    if room.dealer != user_id:
+        return "You are not the dealer", 400
+    if room.player is None:
+        return "Room is empty", 400
+    return "OK"
+
+
+@app.route('/<test>')
+def index(test):
+    print(app.config['SQLALCHEMY_DATABASE_URI'])
+    print(test)
+    # return status code 200
+    return test
 
 
 # @app.route('/signup', methods=['POST', 'GET'])
@@ -79,10 +129,3 @@ def index():
 #     return render_template('login.html')
 
 
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
-#     # users = db.session.query(User).all()
-#     #user_to_delete = users[0]
-#     #session.delete(user_to_delete)
-#     #db.session.commit()
--- db.py
+++
@@ -1,5 +0,0 @@
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.Sring, unique=True)
-    password = db.Column(db.Sring)
-    email = db.Column(db.Sring, unique=True)
